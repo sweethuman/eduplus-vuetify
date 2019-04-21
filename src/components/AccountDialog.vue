02b22a9a13c75e597f63b0f59850513c3c639f22
Avram Gheorghe <hackscomicon@yahoo.com>
2019-04-21 19:21:19 +0300

    Improve AccountDialog Background

--- src/components/AccountDialog.vue
+++ src/components/AccountDialog.vue
@@ -4,7 +4,6 @@
     max-width="500"
     :fullscreen="!this.$vuetify.breakpoint.mdAndUp"
     :transition="this.$vuetify.breakpoint.mdAndUp ? 'dialog-transition' : 'dialog-bottom-transition'"
-    content-class="white"
   >
     <template #activator="{ on }">
       <v-btn class="orange-gradient" flat large v-on="on">
@@ -12,99 +11,101 @@
         Login
       </v-btn>
     </template>
-    <v-tabs
-      v-model="activeTab"
-      color="#F27121"
-      dark
-      slider-color="#E94057"
-      grow
-      icons-and-text
-      style="background: white;"
-    >
-      <v-tab ripple class="wiretap-gradient-reversed">
-        Login
-        <v-icon>{{ tabIcon(0, "mdi-account") }}</v-icon>
-      </v-tab>
-      <v-btn
-        v-if="!this.$vuetify.breakpoint.mdAndUp"
-        style="margin: auto"
-        class="wiretap-gradient-halved"
-        round
-        @click="dialog = false"
-      >
-        <v-icon>mdi-close</v-icon>
-      </v-btn>
-      <v-tab ripple class="wiretap-gradient">
-        Register
-        <v-icon>{{ tabIcon(1, "mdi-account-plus") }}</v-icon>
-      </v-tab>
-      <v-tab-item>
-        <v-card flat>
-          <v-card-text>
-            <v-form v-model="validLogin">
-              <v-text-field
-                v-model="loginId"
-                prepend-icon="mdi-account"
-                name="loginId"
-                label="Username/Email"
-                type="text"
-                color="orange"
-                :rules="loginIdRules"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="loginPassword"
-                prepend-icon="mdi-lock"
-                name="password"
-                label="Password"
-                type="password"
-                color="red"
-                :rules="passwordRules"
-                required
-              ></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" class="wiretap-gradient-angled" large @click="dialog = false">Login </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-tab-item>
-      <v-tab-item>
-        <v-card flat>
-          <v-card-text>
-            <v-form ref="form">
-              <v-text-field
-                prepend-icon="mdi-account"
-                name="username"
-                label="Username"
-                type="text"
-                color="orange"
-              ></v-text-field>
-              <v-text-field prepend-icon="mdi-at" name="email" label="Email" type="text" color="#FF8C00"></v-text-field>
-              <v-text-field
-                prepend-icon="mdi-lock"
-                name="password"
-                label="Password"
-                type="password"
-                color="red"
-              ></v-text-field>
-              <v-text-field
-                prepend-icon="mdi-lock"
-                name="passwordRepeat"
-                label="Repeat Password"
-                type="password"
-                color="#8b0000"
-              ></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" class="wiretap-gradient-angled" large @click="dialog = false">Create Account </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-tab-item>
-    </v-tabs>
+    <v-card>
+      <v-tabs v-model="activeTab" color="#F27121" dark slider-color="#E94057" grow icons-and-text>
+        <v-tab ripple class="wiretap-gradient-reversed">
+          Login
+          <v-icon>{{ tabIcon(0, "mdi-account") }}</v-icon>
+        </v-tab>
+        <v-btn
+          v-if="!this.$vuetify.breakpoint.mdAndUp"
+          style="margin: auto"
+          class="wiretap-gradient-halved"
+          round
+          @click="dialog = false"
+        >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-tab ripple class="wiretap-gradient">
+          Register
+          <v-icon>{{ tabIcon(1, "mdi-account-plus") }}</v-icon>
+        </v-tab>
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>
+              <v-form v-model="validLogin">
+                <v-text-field
+                  v-model="loginId"
+                  prepend-icon="mdi-account"
+                  name="loginId"
+                  label="Username/Email"
+                  type="text"
+                  color="orange"
+                  :rules="loginIdRules"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="loginPassword"
+                  prepend-icon="mdi-lock"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  color="red"
+                  :rules="passwordRules"
+                  required
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" class="wiretap-gradient-angled" large @click="dialog = false">Login </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>
+              <v-form ref="form">
+                <v-text-field
+                  prepend-icon="mdi-account"
+                  name="username"
+                  label="Username"
+                  type="text"
+                  color="orange"
+                ></v-text-field>
+                <v-text-field
+                  prepend-icon="mdi-at"
+                  name="email"
+                  label="Email"
+                  type="text"
+                  color="#FF8C00"
+                ></v-text-field>
+                <v-text-field
+                  prepend-icon="mdi-lock"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  color="red"
+                ></v-text-field>
+                <v-text-field
+                  prepend-icon="mdi-lock"
+                  name="passwordRepeat"
+                  label="Repeat Password"
+                  type="password"
+                  color="#8b0000"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" class="wiretap-gradient-angled" large @click="dialog = false"
+                >Create Account
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
   </v-dialog>
 </template>
 <script>
