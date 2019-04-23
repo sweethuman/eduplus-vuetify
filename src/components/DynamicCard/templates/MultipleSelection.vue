e34e3bb5a51b7a4ba26a30ee438a8a1aef498822
Avram Gheorghe <hackscomicon@yahoo.com>
2019-04-23 18:30:44 +0300

    Use Mixin For DynamicCard Children

--- src/components/DynamicCard/templates/MultipleSelection.vue
+++ src/components/DynamicCard/templates/MultipleSelection.vue
@@ -42,44 +42,17 @@
 </template>
 
 <script>
+import { examSelectionMixin } from "../../../mixins/examSelectionMixin";
+
 export default {
   name: "MultipleSelection",
-  props: {
-    exercise: {
-      type: Object,
-      default: undefined,
-      required: true,
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-  },
+  mixins: [examSelectionMixin],
   data() {
     return {
       checkboxSelection: [],
-      chipStore: [],
-      resultOptions: Object.freeze({ correct: 0, wrong: 1, intermediate: 2 }),
     };
   },
-  beforeMount() {
-    //Sets initial chip values to disabled(false)
-    //Chips are the indicators next to the label that say if you're answer is correct or not
-    this.calculateChips();
-  },
   methods: {
-    getLabelText(value) {
-      if (value === this.resultOptions.correct) return "Corect";
-      if (value === this.resultOptions.wrong) return "Gresit";
-      if (value === this.resultOptions.intermediate) return "Raspunsul Corect";
-      return "";
-    },
-    getAnswerColor(value) {
-      if (value === this.resultOptions.correct) return "#c6e377";
-      if (value === this.resultOptions.wrong) return "#ef6c57";
-      if (value === this.resultOptions.intermediate) return "#c6e377";
-      return "";
-    },
     showChip(chipId) {
       if (this.checkboxSelection.includes(chipId)) {
         if (this.exercise.correctAnswer.includes(chipId)) {
@@ -92,17 +65,6 @@ export default {
         return { result: true, value: this.resultOptions.intermediate };
       }
       return { result: false };
-    },
-    calculateChips() {
-      let points = 0;
-      for (let j = 0; j < this.exercise.options.length; j++) {
-        let tempChipValue = this.showChip(j);
-        if (tempChipValue.result === true && tempChipValue.value === this.resultOptions.correct) {
-          ++points;
-        }
-        this.$set(this.chipStore, j, tempChipValue);
-      }
-      return points;
     },
     isAnswerChecked() {
       return this.checkboxSelection.length !== 0;
