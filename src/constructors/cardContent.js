ae53686fe8e7a9a920755836eb3181c71711fdd4
Avram Gheorghe <hackscomicon@yahoo.com>
2019-05-05 13:22:01 +0300

    Add Card for Disciplines

---
+++ src/constructors/cardContent.js
@@ -0,0 +1,6 @@
+export default function CardContent(title, description, photo, lazyPhoto) {
+  this.title = title;
+  this.description = description;
+  this.photo = photo;
+  this.lazyPhoto = lazyPhoto;
+}
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import "./registerServiceWorker";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import "./css/gradients.css";
+import "./css/utility.css";
 import store from "./store";
 
 Vue.config.productionTip = false;
