d926a21be6e6507e75cb02549b2c5af4376b3567
Avram Gheorghe <hackscomicon@yahoo.com>
2019-05-16 17:54:24 +0300

    Update ExamResult with Multiple Messages

--- src/components/ExamResult.vue
+++ src/components/ExamResult.vue
@@ -1,12 +1,19 @@
 <template>
   <v-card>
     <v-card-text>
-      <h2 class="display-3 font-weight-regular mb-3">Ai terminat testul!</h2>
+      <h2 v-if="text != null" class="display-3 font-weight-regular mb-3">{{ text }}</h2>
       <h3 class="display-2">
-        Ai
+        Pe Subiect ai
         <span class="red--text">{{ points }}</span>
         puncte din
         <span class="green--text">{{ maxPoints }}</span>
+        puncte!
+      </h3>
+      <h3 v-if="allPoints != null && maxAllPoints != null" class="display-2">
+        In Total ai
+        <span class="red--text">{{ allPoints }}</span>
+        puncte din
+        <span class="green--text">{{ maxAllPoints }}</span>
         puncte!
       </h3>
     </v-card-text>
@@ -17,8 +24,26 @@
 export default {
   name: "ExamResult",
   props: {
-    points: Number,
-    maxPoints: Number,
+    points: {
+      type: Number,
+      required: true,
+    },
+    maxPoints: {
+      type: Number,
+      required: true,
+    },
+    allPoints: {
+      type: Number,
+      default: null,
+    },
+    maxAllPoints: {
+      type: Number,
+      default: null,
+    },
+    text: {
+      type: String,
+      default: null,
+    },
   },
 };
 </script>
