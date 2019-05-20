7c45cfc3f0894757ef78014e40198c5f3a8eee8b
Avram Gheorghe <hackscomicon@yahoo.com>
2019-05-20 18:47:10 +0300

    Version 0.1.1

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,7 @@
         <v-card-actions class="justify-center">
           <span class="subheading">
             Versiunea
-            <strong>0.1 Alpha</strong>
+            <strong>0.1.1 Alpha</strong>
           </span>
           <span class="mx-2">—</span>
           <span class="subheading">
