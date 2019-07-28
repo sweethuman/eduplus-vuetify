6589aa64183ddec50b5baee8b3c800162558b75c
sweethuman <hackscomicon@yahoo.com>
2019-07-28 15:43:37 +0300

    Get Exams from Firestore

--- src/views/Exam.vue
+++ src/views/Exam.vue
@@ -5,19 +5,24 @@
         <h2 id="examTitle" class="display-3 my-5">{{ examData.title }}</h2>
       </v-flex>
       <card-list-exam
-        v-if="examData.exam != null && !notFound"
+        v-if="pages != null && !notFound && pages.length !== 0"
         :key="$route.params.name"
-        :exam-data="examData.exam"
+        :exam-data="pages"
         @page-change="page = $event"
       />
+      <item-not-found
+        v-else-if="!$wait.is('loading exam')"
+        :text="'Paginile pentru examenul ' + $route.params.name + ' nu au fost gasite!'"
+      ></item-not-found>
     </v-layout>
-    <item-not-found v-if="notFound" :text="$route.params.name + ' examen nu a fost gasit!'"></item-not-found>
+    <item-not-found v-else :text="$route.params.name + ' examen nu a fost gasit!'"></item-not-found>
   </v-container>
 </template>
 
 <script>
 import CardListExam from "../components/CardListExam";
 import ItemNotFound from "../components/ItemNotFound";
+import { firestore } from "../firebase";
 
 export default {
   name: "Exam",
@@ -27,6 +32,7 @@ export default {
       examData: {
         title: "Loading...",
       },
+      pages: [],
       notFound: false,
       page: 0,
     };
@@ -47,11 +53,24 @@ export default {
     async loadExam(routeObject) {
       this.$wait.start("loading exam");
       try {
-        let exam = await import(`../data/exams/${routeObject.params.name}.json`);
-        this.examData = exam.default;
-        this.notFound = false;
+        let dataSnapshot = await firestore
+          .collection("exams")
+          .doc(routeObject.params.name)
+          .get();
+        if (dataSnapshot.exists) {
+          this.examData = dataSnapshot.data();
+          let querySnapshot = await firestore
+            .collection(`/exams/${routeObject.params.name}/pages`)
+            .orderBy("subjectIndex", "asc")
+            .get();
+          this.pages = querySnapshot.docs.map(doc => doc.data());
+          this.notFound = false;
+        } else {
+          this.notFound = true;
+        }
       } catch (e) {
         this.notFound = true;
+        this.$log.error(e);
       }
       this.$wait.end("loading exam");
     },
